ea74e3af8dcd6cadb4354273efbeac993b84b755
Rani Shah <rani76@gmail.com>
2018-03-19 15:36:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,16 @@ class Song
 
   def initialize
     @name = @name
-    @artist = artist 
+    @artist = artist
+    @@all << self
   end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end   
 
 end
