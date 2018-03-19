4b4ae6578b4295bcc3c569dd99fef63e84b02311
Rani Shah <rani76@gmail.com>
2018-03-19 15:38:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,16 +6,24 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count += 1 
-  end
-
-  def songs
-    @songs
+    @@song_count += 1
   end
 
   def add_song(song)
     self.songs << song
     song.artist = self
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def songs
+    @songs
   end
 
   def self.song_count
